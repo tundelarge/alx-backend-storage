0134d5aea24b635bc13c8706cbb51582d8e2c1b9
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-13 22:17:01 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,4 @@
-qwertyu
+-- script that ranks country origins of bands, ordered
+-- by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
