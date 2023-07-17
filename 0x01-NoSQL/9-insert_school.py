96b29fe34ad1a2954c7483bd4d7969a8acdd67fc
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-17 11:42:53 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,12 @@
-qwertyu
+#!/usr/bin/env python3
+"""
+This module have a utility function that insert documents
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert into school
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
