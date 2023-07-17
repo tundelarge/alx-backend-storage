a0f0432dc9f614d9d2d4d899f189bc2cd657f338
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-17 11:43:35 +0100

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,15 @@
-qwertyu
+#!/usr/bin/env python3
+"""
+change school topic
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
