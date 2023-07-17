e95d6d2b6e8160f161c86b0cc86bd6d1e3d5e75a
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-17 11:47:07 +0100

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,23 @@
-qwertyu
+#!/usr/bin/env python3
+"""
+students
+"""
+
+
+def top_students(mongo_collection):
+    """ students by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
